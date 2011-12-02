f50de5d84847f3f6429051c541716f0043c3b3b3
Chris Lizon <chris@lizon.ca>
2011-12-02 13:18:38 -0500

    Set up the classes for the switchview

---
+++ MemoryGame/SwitchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SwitchViewController.h
+//  MemoryGame
+//
+//  Created by Guest User on 11-12-02.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SwitchViewController : UIViewController
+
+@end
---
+++ MemoryGame/SwitchViewController.m
@@ -0,0 +1,13 @@
+//
+//  SwitchViewController.m
+//  MemoryGame
+//
+//  Created by Guest User on 11-12-02.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SwitchViewController.h"
+
+@implementation SwitchViewController
+
+@end
