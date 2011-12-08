1a84e214618eeee8be48068599dc1262307b6481
Guest User <Guest1@UOITs-Mac-mini.local>
2011-12-08 11:57:04 -0500

    Added start of Basic info screen

---
+++ MemoryGame/InfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  InfoViewController.h
+//  MemoryGame
+//
+//  Created by Guest User on 11-12-08.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoViewController : UIViewController
+
+@end
