1dcb028b10fbb36efb4fca34e2133c119cfdf48d
Ben Waters <ben25890@gmail.com>
2012-02-04 00:40:44 -0500

    GNU GPL v3

--- MemoryGame/AppDelegate.h
+++ MemoryGame/AppDelegate.h
@@ -3,8 +3,22 @@
 //  MemoryGame
 //
 //  Created by Guest User on 11-11-15.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
-//
+/*
+This file is part of iOS-Memory-Game.
+
+    iOS-Memory-Game is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    iOS-Memory-Game is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with iOS-Memory-Game.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
